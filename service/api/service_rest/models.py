4de18eb5e68b9a665dc6f08f47052b0545ca8e86
Caleb <cmlaping@gmail.com>
2023-01-24 15:28:59 -0700

    Installed service application and created service models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.IntegerField(unique=True)
+
+
+class Appointment(models.Model):
+    owner = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    reason = models.TextField()
+
+    vehicle = models.ForeignKey(
+        AutomobileVO,
+        related_name='appointments',
+        on_delete=models.PROTECT,
+    )
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name='appointments',
+        on_delete=models.PROTECT,
+    )
